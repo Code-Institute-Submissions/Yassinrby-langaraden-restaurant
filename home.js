b6b7d412e248f1c772f621cbec40e6970d1777cd
Yassinrby <yassinrby@outlook.com>
2020-08-04 15:35:24 +0000

    Made some changes in home page and having some trouble targetting with javascript. Will finish all html and css before working on Javascript

--- home.js
+++ home.js
@@ -1,6 +1,3 @@
-$('.navbar-nav a').click(function(){
-    $('#nav a').css('text-decoration', 'none', function(){
-        $(this).css('text-decoration', 'underline');
-    });
-    return;
-});
+
+    document.getElementById('#phone').onclick = '0720036761';
+    
