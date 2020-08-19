88d185d97017a8efd293ae72f5a7c6136909eb09
Yassinrby <yassinrby@outlook.com>
2020-08-19 17:56:48 +0000

    Google maps API complete

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,16 @@
 function initMap() {
-  let langaraden = { lat: 59.34728, lng: 18.324622 };
+  var langaraden = { lat: 59.34728, lng: 18.324622 };
 
-  let map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: langaraden,
+  var map = new google.maps.Map(document.getElementById("map"), {zoom: 15, center: langaraden,
   });
 
-  let marker = new google.maps.Marker({ postition: langaraden, map: map });
+  var marker = new google.maps.Marker({ 
+      postition: langaraden, 
+      map: map,
+    });
+
+    marker.setMap(map);
+
 }
+
+
