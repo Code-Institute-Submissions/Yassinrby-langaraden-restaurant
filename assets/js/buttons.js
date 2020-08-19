1920b70eabd7b85ca6556e1c548c6fbe16f0546f
Yassinrby <yassinrby@outlook.com>
2020-08-19 15:30:58 +0000

    Menu complete, added hightlight class on selected meal and changed the line class

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -9,18 +9,21 @@ function myFunction() {
 
 $(document).ready(function () {
   $("#lunch-btn-veg").click(function () {
+      $(".veg-lunch").toggleClass("highlight");
     $(".fish-lunch, .meat-lunch").toggleClass("line");
   });
 });
 
 $(document).ready(function () {
   $("#lunch-btn-fish").click(function () {
+      $(".fish-lunch").toggleClass("highlight");
     $(".veg-lunch, .meat-lunch").toggleClass("line");
   });
 });
 
 $(document).ready(function () {
   $("#lunch-btn-meat").click(function () {
+      $(".meat-lunch").toggleClass("highlight");
     $(".veg-lunch, .fish-lunch").toggleClass("line");
   });
 });
@@ -29,18 +32,21 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $("#dinner-btn-veg").click(function () {
+      $(".veg-dinner").toggleClass("highlight");
     $(".meat-dinner, .fish-dinner").toggleClass("line");
   });
 });
 
 $(document).ready(function () {
   $("#dinner-btn-fish").click(function () {
+      $(".fish-dinner").toggleClass("highlight");
     $(".meat-dinner, .veg-dinner").toggleClass("line");
   });
 });
 
 $(document).ready(function () {
   $("#dinner-btn-meat").click(function () {
+      $(".meat-dinner").toggleClass("highlight");
     $(".fish-dinner, .veg-dinner").toggleClass("line");
   });
 });
